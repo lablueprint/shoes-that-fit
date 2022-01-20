b5b3d3f2074932986d9b2d57dc5b1fc4cbc16f98
Laura Lu <81529754+snlaurax@users.noreply.github.com>
2022-01-19 21:36:38 -0500

    Basic order form outline
    Storing quantity and shoe brand as arrays and storing the two arrays in a larger array called shoe

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import './styles/App.css';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components';
 import { Home, MainInventory } from './pages';
+import Orderform from './components/orderform';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Orderform />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/inventory" element={<MainInventory />} />
       </Routes>
     </div>
+
   );
 }
 
