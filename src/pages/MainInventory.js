10e9fbdcba75753c94aae3678c8073686d1a18fd
Siddharth Nandy <sidd.nandy@gmail.com>
2022-01-19 20:27:41 -0800

    Airtable integration for inventory table
    Filter function has a bug

--- src/pages/MainInventory.js
+++ src/pages/MainInventory.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
 // airtable configuration
-// const Airtable = require('airtable');
+const Airtable = require('airtable');
 
-// const airtableConfig = {
-//   apiKey: process.env.REACT_APP_AIRTABLE_USER_KEY,
-//   baseKey: process.env.REACT_APP_AIRTABLE_BASE_KEY,
-// };
+const airtableConfig = {
+  apiKey: process.env.REACT_APP_AIRTABLE_USER_KEY,
+  baseKey: process.env.REACT_APP_AIRTABLE_BASE_KEY,
+};
 
-// const base = new Airtable({ apiKey: airtableConfig.apiKey })
-//   .base(airtableConfig.baseKey);
+const base = new Airtable({ apiKey: airtableConfig.apiKey })
+  .base(airtableConfig.baseKey);
 
 function MainInventory() {
   const [rows, setRows] = useState([]);
@@ -19,48 +19,48 @@ function MainInventory() {
   const [value, setValue] = useState('');
 
   const getInventory = () => {
-    // base('Inventory').select({ view: 'Grid view' }).all()
-    //   .then((records) => {
-    //     setRows(records);
-    //   });
-    setRows([
-      {
-        id: 'recdSsNu3GksmtYY0',
-        fields: {
-          'Client Name': 'Shoes That Fit Warehouse',
-          'Location Name': 'STF Warehouse',
-          'Bin Name': 'Area 51',
-          'Part Name': 'B1',
-          'Part Description': 'B1',
-          Quantity: 78,
-        },
-        createdTime: '2022-01-13T04:35:45.000Z',
-      },
-      {
-        id: 'recBDHujkX5A8zeCb',
-        fields: {
-          'Client Name': 'Shoes That Fit Warehouse',
-          'Location Name': 'STF Warehouse',
-          'Bin Name': 'Area 51',
-          'Part Name': 'B1.5',
-          'Part Description': 'B1.5',
-          Quantity: 69,
-        },
-        createdTime: '2022-01-13T04:35:45.000Z',
-      },
-      {
-        id: 'recexWd0kcC0GMXXl',
-        fields: {
-          'Client Name': 'Shoes That Fit Warehouse',
-          'Location Name': 'STF Warehouse',
-          'Bin Name': 'Area 51',
-          'Part Name': 'B10',
-          'Part Description': 'B10',
-          Quantity: 24,
-        },
-        createdTime: '2022-01-13T04:35:45.000Z',
-      },
-    ]);
+    base('Current Item Inventory (All Locations 1.3.2022)').select({ view: 'Grid view' }).all()
+      .then((records) => {
+        setRows(records);
+      });
+    // setRows([
+    //   {
+    //     id: 'recdSsNu3GksmtYY0',
+    //     fields: {
+    //       'Client Name': 'Shoes That Fit Warehouse',
+    //       'Location Name': 'STF Warehouse',
+    //       'Bin Name': 'Area 51',
+    //       'Part Name': 'B1',
+    //       'Part Description': 'B1',
+    //       Quantity: 78,
+    //     },
+    //     createdTime: '2022-01-13T04:35:45.000Z',
+    //   },
+    //   {
+    //     id: 'recBDHujkX5A8zeCb',
+    //     fields: {
+    //       'Client Name': 'Shoes That Fit Warehouse',
+    //       'Location Name': 'STF Warehouse',
+    //       'Bin Name': 'Area 51',
+    //       'Part Name': 'B1.5',
+    //       'Part Description': 'B1.5',
+    //       Quantity: 69,
+    //     },
+    //     createdTime: '2022-01-13T04:35:45.000Z',
+    //   },
+    //   {
+    //     id: 'recexWd0kcC0GMXXl',
+    //     fields: {
+    //       'Client Name': 'Shoes That Fit Warehouse',
+    //       'Location Name': 'STF Warehouse',
+    //       'Bin Name': 'Area 51',
+    //       'Part Name': 'B10',
+    //       'Part Description': 'B10',
+    //       Quantity: 24,
+    //     },
+    //     createdTime: '2022-01-13T04:35:45.000Z',
+    //   },
+    // ]);
   };
   const handleFilterChange = (e, filterType) => {
     // changes state
