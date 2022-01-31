778b3aaf6a0b76280025b36e9f9bbf63c628b75d
Michael Shi <69874869+mykulls@users.noreply.github.com>
2022-01-31 15:20:45 -0800

    Remove OrderForm from home screen

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components';
 // import Card from './components/card';
 import { Home, MainInventory, NewShoeForm } from './pages';
-import OrderForm from './components/orderform';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <OrderForm />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/inventory" element={<MainInventory />} />
