5fd34ad1d8d238a45eb085e107c79064e511b561
Solaine Zhao <solainezh2019@gmail.com>
2022-01-30 20:34:34 -0800

    create airtable records for users through sign up page

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,33 +3,57 @@ import React, { useState } from 'react';
 // import React from 'react';
 // import { makeStyles } from '@material-ui/core/styles';
 
+const Airtable = require('airtable');
+
+const airtableConfig = {
+  apiKey: process.env.REACT_APP_AIRTABLE_USER_KEY,
+  baseKey: process.env.REACT_APP_AIRTABLE_BASE_KEY,
+};
+
+const base = new Airtable({ apiKey: airtableConfig.apiKey }).base(airtableConfig.baseKey);
+
 export default function LoginPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  // const [isEducator, setRole] = useState(True);
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [hasAccount, setHasAccount] = useState(false);
 
-  const onSubmit = async (e) => {
+  const handleSignup = async (e) => {
     e.preventDefault();
-    console.log(username);
-    console.log(password);
+    if (password === confirmPassword) {
+      base('Users').create(
+        [
+          {
+            fields: {
+              Username: username,
+              Password: password,
+              // Role: role,
+            },
+          }],
+        (err, records) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          records.forEach((record) => {
+            console.log(record.getId());
+          });
+        },
+      );
+    } else {
+      console.log("passwords don't match");
+    }
   };
 
-  // const useStyles = makeStyles({
-
-  //   loginWrapper: {
-  //     display: 'flex',
-  //     flexDirection: 'column',
-  //     alignItems: 'center',
-  //   },
-  // });
-
-  // const classes = useStyles();
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+  };
 
   return (
-    // <div className={classes.loginWrapper}>
     <div className="loginWrapper">
       <h1>Please Log In</h1>
-      <form onSubmit={onSubmit}>
+      {/* <form onSubmit={onSubmit}> */}
+      <form>
         <label>
           <p>Username</p>
           <input type="text" onChange={(e) => setUsername(e.target.value)} />
@@ -38,8 +62,40 @@ export default function LoginPage() {
           <p>Password</p>
           <input type="password" onChange={(e) => setPassword(e.target.value)} />
         </label>
+        <label>
+          <p>Confirm Password</p>
+          <input type="password" onChange={(e) => setConfirmPassword(e.target.value)} />
+        </label>
         <div>
-          <button type="submit">Submit</button>
+          {hasAccount ? (
+            <>
+              <button type="button" onClick={handleSignIn}> Sign In</button>
+              <p className="accountStatusParagraph">
+                No account?
+                {/* toggle the state when you click the button */}
+                <button
+                  type="button"
+                  onClick={() => setHasAccount(!hasAccount)}
+                >
+                  Sign up
+                </button>
+              </p>
+            </>
+          ) : (
+            <>
+              <button type="button" onClick={handleSignup}> Sign up</button>
+              <p className="accountStatusParagraph">
+                {' '}
+                Have an account?
+                <button
+                  type="button"
+                  onClick={() => setHasAccount(!hasAccount)}
+                >
+                  Sign In
+                </button>
+              </p>
+            </>
+          )}
         </div>
       </form>
     </div>
