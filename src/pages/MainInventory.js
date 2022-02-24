15df3064d928b4f466cf4f8d23dda7e6ba896423
Siddharth Nandy <sidd.nandy@gmail.com>
2022-02-23 23:22:35 -0800

    Implemented category filters with dropdown menus
    Todo: filter for Quantity category

--- src/pages/MainInventory.js
+++ src/pages/MainInventory.js
@@ -47,14 +47,48 @@ const base = Airtable.base(process.env.REACT_APP_AIRTABLE_BASE_KEY);
 function MainInventory() {
   const [rows, setRows] = useState([]);
   const [items, setItems] = useState([]);
-  const [category, setCategory] = useState('all');
-  const [value, setValue] = useState('');
-  const [binCounts, setBinCounts] = useState({});
-  const [binOptions, setBinOptions] = useState([]);
+  // const [c, setCategory] = useState('all');
+  // const [v, setValue] = useState('');
+  // const [binCounts, setBinCounts] = useState({});
+  // const [binOptions, setBinOptions] = useState([]);
   const [inventoryTotal, setInventoryTotal] = useState(0);
-  const [binOptionsSelected, setBinOptionsSelected] = useState([]);
-  // const [expanded, setExpanded] = useState(false);
-  // const [partCounts, setPartCounts] = useState({});
+  // const [binOptionsSelected, setBinOptionsSelected] = useState([]);
+  const [updateFilter, setUpdateFilter] = useState(false);
+  const [categoryOptions, setCategoryOptions] = useState({
+    'Client Name': [],
+    'Location Name': [],
+    'Bin Name': [],
+    'Part Name': [],
+    'Part Description': [],
+    Quantity: [],
+  });
+  const [optionsSelected, setOptionsSelected] = useState({
+    'Client Name': [],
+    'Location Name': [],
+    'Bin Name': [],
+    'Part Name': [],
+    'Part Description': [],
+    Quantity: [],
+  });
+
+  const categories = ['Client Name', 'Location Name', 'Bin Name', 'Part Name', 'Part Description', 'Quantity'];
+  const filterableCategories = ['Client Name', 'Location Name', 'Bin Name', 'Part Name', 'Part Description'];
+  // setCategoryOptions({
+  //   'Client Name': [],
+  //   'Location Name': [],
+  //   'Bin Name': [],
+  //   'Part Name': [],
+  //   'Part Description': [],
+  //   Quantity: [],
+  // });
+  // setOptionsSelected({
+  //   'Client Name': [],
+  //   'Location Name': [],
+  //   'Bin Name': [],
+  //   'Part Name': [],
+  //   'Part Description': [],
+  //   Quantity: [],
+  // });
 
   const getInventory = () => {
     base('Current Item Inventory (All Locations 1.3.2022)').select({ view: 'Grid view' }).all()
@@ -62,68 +96,82 @@ function MainInventory() {
         setRows(records);
       });
   };
-  const handleFilterChange = (e, filterType) => {
-    e.preventDefault();
-    switch (filterType) {
-      case 'category':
-        setCategory(e.target.value);
-        break;
-      case 'value':
-        setValue(e.target.value);
-        break;
-      default: break;
-    }
+  const createOptions = (category, optionList) => {
+    categoryOptions[category] = optionList;
+    setCategoryOptions(categoryOptions);
   };
-  const handleSubmission = (e) => {
-    e.preventDefault();
+  const handleOptionSelection = (e, category) => {
+    optionsSelected[category] = e;
+    setOptionsSelected(optionsSelected);
+    setUpdateFilter(!updateFilter);
   };
+  // const handleFilterChange = (e, filterType) => {
+  //   e.preventDefault();
+  //   switch (filterType) {
+  //     case 'category':
+  //       setCategory(e.target.value);
+  //       break;
+  //     case 'value':
+  //       setValue(e.target.value);
+  //       break;
+  //     default: break;
+  //   }
+  // };
+  // const handleSubmission = (e) => {
+  //   e.preventDefault();
+  // };
 
   useEffect(getInventory, []);
+
+  // Retrieves number of entries for each bin and
+  // Processes the binCounts into a list of options to be selected on the frontend
   useEffect(() => {
-    const counts = {};
-    for (let i = 0; i < rows.length; i += 1) {
-      counts[rows[i].fields['Bin Name']] = 1 + (counts[rows[i].fields['Bin Name']] || 0);
-    }
-    setBinCounts(counts);
+    categories.forEach((category) => {
+      const counts = {};
+      for (let i = 0; i < rows.length; i += 1) {
+        counts[rows[i].fields[category]] = 1 + (counts[rows[i].fields[category]] || 0);
+      }
+      const optionList = [];
+      Object.keys(counts).forEach((key) => optionList.push({
+        value: key,
+        label: (`${key} - ${counts[key]}`),
+      }));
+      createOptions(category, optionList);
+    });
   }, [rows]);
 
-  useEffect(() => {
-    const optionList = [];
-    Object.keys(binCounts).forEach((key) => optionList.push({
-      value: key,
-      label: (`${key} - ${binCounts[key]}`),
-    }));
-    setBinOptions(optionList);
-  }, [binCounts]);
+  // useEffect(() => {
+  //   let filteredProducts = rows;
+  //   if (v !== '') {
+  //     if (c !== 'all' && c !== 'Quantity') {
+  // eslint-disable-next-line max-len
+  //       filteredProducts = filteredProducts.filter((item) => (String(item.fields[c]).toLowerCase()).includes((String(v)).toLowerCase()));
+  //     } else if (c === 'Quantity') {
+  //       // eslint-disable-next-line max-len
+  //       filteredProducts = filteredProducts.filter((item) => (item.fields.Quantity >= v));
+  //     }
+  //   }
+  //   // console.log(filteredProducts);
+  //   setItems(filteredProducts);
+  // }, [c, v, rows]);
 
+  // Applies a filter when a bin option is selected
   useEffect(() => {
     let filteredProducts = rows;
-    if (value !== '') {
-      if (category !== 'all' && category !== 'Quantity') {
-        // eslint-disable-next-line max-len
-        filteredProducts = filteredProducts.filter((item) => (String(item.fields[category]).toLowerCase()).includes((String(value)).toLowerCase()));
-      } else if (category === 'Quantity') {
-        // eslint-disable-next-line max-len
-        filteredProducts = filteredProducts.filter((item) => (item.fields.Quantity >= value));
-      }
-    }
-    // console.log(filteredProducts);
-    setItems(filteredProducts);
-  }, [category, value, rows]);
-  useEffect(() => {
-    let filteredProducts = rows;
-    if (binOptionsSelected.length !== null && binOptionsSelected.length > 0) {
-      filteredProducts = filteredProducts.filter((item) => {
-        let include = false;
-        binOptionsSelected.forEach((option) => {
-          // eslint-disable-next-line max-len
-          include = include || (String(item.fields['Bin Name']).toLowerCase()).includes((String(option.value)).toLowerCase());
+    filterableCategories.forEach((category) => {
+      if (optionsSelected[category].length > 0) {
+        filteredProducts = filteredProducts.filter((item) => {
+          let include = false;
+          optionsSelected[category].forEach((option) => {
+            // eslint-disable-next-line max-len
+            include = include || (String(item.fields[category]).toLowerCase()).localeCompare((String(option.value)).toLowerCase()) === 0;
+          });
+          return include;
         });
-        return include;
-      });
-    }
+      }
+    });
     setItems(filteredProducts);
-  }, [binOptionsSelected]);
+  }, [optionsSelected, rows, updateFilter]);
 
   useEffect(() => {
     let sum = 0;
@@ -135,46 +183,40 @@ function MainInventory() {
 
   return (
     <>
-      <div>
+      {/* <div>
         <form className="filter" onSubmit={(e) => handleSubmission(e)}>
           <select name="category" id="category" onChange={(e) => handleFilterChange(e, 'category')}>
             <option value="all">All</option>
-            <option value="Client Name">Client Name</option>
-            <option value="Location Name">Location Name</option>
-            <option value="Bin Name">Bin Name</option>
-            <option value="Part Name">Part Name</option>
-            <option value="Part Description">Part Description</option>
-            <option value="Quantity">Quantity</option>
+            {categories.map((category) => (
+              <option value={category}>{category}</option>
+            ))}
           </select>
-          <input name="value" onChange={(e) => handleFilterChange(e, 'value')} placeholder="Search For" />
+    <input name="value" onChange={(e) => handleFilterChange(e, 'value')} placeholder="Search For" />
         </form>
-      </div>
+      </div> */}
       <table>
         <thead>
-          <th>Client</th>
-          <th>Location</th>
-          <th>
-            Bin
-            <ReactSelect
-              isMulti
-              defaultValue={binOptionsSelected}
-              onChange={setBinOptionsSelected}
-              options={binOptions}
-            />
-          </th>
-          <th>Part Name</th>
-          <th>Part Description</th>
-          <th>Quantity</th>
+          <tr>
+            {filterableCategories.map((category) => (
+              <th>
+                {category}
+                <ReactSelect
+                  isMulti
+                  onChange={(e) => handleOptionSelection(e, category)}
+                  options={categoryOptions[category]}
+                  placeholder={category}
+                />
+              </th>
+            ))}
+            <th>Quantity</th>
+          </tr>
         </thead>
         <tbody>
           {items.map((row) => (
             <tr>
-              <td>{row.fields['Client Name']}</td>
-              <td>{row.fields['Location Name']}</td>
-              <td>{row.fields['Bin Name']}</td>
-              <td>{row.fields['Part Name']}</td>
-              <td>{row.fields['Part Description']}</td>
-              <td>{row.fields.Quantity}</td>
+              {categories.map((category) => (
+                <td>{row.fields[category]}</td>
+              ))}
             </tr>
           ))}
         </tbody>
