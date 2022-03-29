14f8c25f9d3686abc1a0a84783a28b07820ac91f
Michael Shi <michshi8@gmail.com>
2022-03-28 21:29:36 -0700

    change env names for airlock

--- src/lib/airtable.js
+++ src/lib/airtable.js
@@ -1,7 +1,7 @@
 import Airtable from '@calblueprint/airlock';
 
-const BASE_ID = process.env.REACT_APP_AIRTABLE_BASE_ID;
-const API_KEY = process.env.REACT_APP_AIRTABLE_API_KEY;
+const BASE_ID = process.env.REACT_APP_AIRTABLE_BASE_KEY;
+const API_KEY = process.env.REACT_APP_AIRTABLE_USER_KEY;
 const ENDPOINT_URL = 'http://localhost:8000';
 
 Airtable.configure({
