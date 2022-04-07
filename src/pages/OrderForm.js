0091eff5f4a2a2331adf223fdaa5499b04ab37b7
Laura <lauralu201@g.ucla.edu>
2022-04-06 23:40:23 -0700

    stylinggg

--- src/pages/OrderForm.js
+++ src/pages/OrderForm.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import print from 'print-js';
 // import reactDom from 'react-dom';
-import Card from '../components/card';
 import './OrderForm.css';
 
 const Airtable = require('airtable');
@@ -11,26 +10,13 @@ const airtableConfig = {
   baseKey: process.env.REACT_APP_AIRTABLE_BASE_KEY,
 };
 
-const base = new Airtable({ apiKey: airtableConfig.apiKey })
-  .base(airtableConfig.baseKey);
+const base = new Airtable({ apiKey: airtableConfig.apiKey }).base(
+  airtableConfig.baseKey,
+);
 
 function OrderForm() {
-  const [cards, setCards] = useState([]);
   const [curcards, setcurCards] = useState([]);
   const [error, setError] = useState();
-  let uniqueActive = [];
-  let uniqueInactive = [];
-
-  const getCards = () => {
-    base('Orders').select({ view: 'Grid view' }).all()
-      .then((records) => {
-        setCards(records);
-      });
-  };
-
-  useEffect(() => {
-    getCards();
-  }, []);
 
   const shoeUpdate = (evt) => {
     evt.preventDefault();
@@ -43,7 +29,12 @@ function OrderForm() {
     card.gender = gender;
 
     let wideWidth = document.getElementById('wideWidth');
-    if (wideWidth === 'y' || wideWidth === 'Y' || wideWidth === 'yes' || wideWidth === 'YES') {
+    if (
+      wideWidth === 'y'
+      || wideWidth === 'Y'
+      || wideWidth === 'yes'
+      || wideWidth === 'YES'
+    ) {
       wideWidth = true;
     } else {
       wideWidth = false;
@@ -58,43 +49,44 @@ function OrderForm() {
 
     const school = document.getElementById('school').value;
     card.school = school;
-    curcards.push(card);
-    setcurCards(curcards);
+
+    const note = document.getElementById('notes').value;
+    card.note = note;
+
+    setcurCards((prev) => [...prev, card]);
   };
 
   function pushToAirtable() {
     curcards.map((card) => {
-      base('Orders').create([
-        {
-          fields: {
-            Name: card.name,
-            Size: card.size,
-            'Teacher/School': card.school,
-            Age: card.age,
-            Gender: card.gender,
-            Wide: card.wideWidth,
-            Active: 'y',
+      base('Orders').create(
+        [
+          {
+            fields: {
+              Name: card.name,
+              Size: card.size,
+              'Teacher/School': card.school,
+              Age: card.age,
+              Gender: card.gender,
+              Wide: card.wideWidth,
+              Active: 'y',
+            },
           },
+        ],
+        (err, records) => {
+          if (err) {
+            console.error(err);
+            setError(<p>{err.message}</p>);
+            console.error(error);
+            return;
+          }
+          records.forEach((record) => {
+            console.log(record.getId());
+          });
         },
-      ], (err, records) => {
-        if (err) {
-          console.error(err);
-          setError(
-            <p>
-              {err.message}
-            </p>,
-          );
-          console.error(error);
-          return;
-        }
-        records.forEach((record) => {
-          console.log(record.getId());
-        });
-      });
+      );
       return 0;
     });
-    setcurCards([]);
-    getCards();
+    // setcurCards([]);
   }
 
   const errorStyle = {
@@ -105,181 +97,152 @@ function OrderForm() {
     print({ printable: 'orders', type: 'html' });
   }
 
-  const getId = () => {
-    uniqueActive = [...new Set(cards.filter((card) => (card.fields.Active === 'y')).map((card) => card.fields.Time))];
-    uniqueInactive = [...new Set(cards.filter((card) => (card.fields.Active === 'n')).map((card) => card.fields.Time))];
-  };
-
   return (
     <div className="row">
-      {getId()}
       <script src="print.js" />
       <div className="column left">
         <div className="left-column">
-          <h1>Add Order: </h1>
           <form onSubmit={shoeUpdate}>
             <label htmlFor="name">
               Student&apos;s First Name & Last Initial:
             </label>
             <div />
-            <input className="full" required type="text" id="name" name="name" />
+            <input
+              className="full"
+              required
+              type="text"
+              id="name"
+              name="name"
+            />
             <div className="space" />
 
             <div className="flex-container">
               <div className="flex-child label">
-                <label htmlFor="age">
-                  Age:
-                  {' '}
-                </label>
+                <label htmlFor="age">Age: </label>
                 <div />
-                <input className="half" required type="number" id="age" name="age" />
+                <input
+                  className="half"
+                  required
+                  type="number"
+                  id="age"
+                  name="age"
+                />
               </div>
               <div className="flex-child label">
-                <label htmlFor="gender">
-                  Gender:
-                  {' '}
-                </label>
+                <label htmlFor="gender">Gender: </label>
                 <div />
-                <input className="half" required type="text" id="gender" name="gender" />
+                <input
+                  className="half"
+                  required
+                  type="text"
+                  id="gender"
+                  name="gender"
+                />
               </div>
             </div>
             <div className="space" />
 
             <div className="flex-container">
               <div className="flex-child label">
-                <label htmlFor="size">
-                  Size:
-                  {' '}
-                </label>
+                <label htmlFor="size">Size: </label>
                 <div />
-                <input className="half" required type="number" id="size" name="size" />
+                <input
+                  className="half"
+                  required
+                  type="number"
+                  id="size"
+                  name="size"
+                />
               </div>
               <div className="flex-child label">
-                <label htmlFor="wideWidth">
-                  Wide Width?:
-                  {' '}
-                </label>
+                <label htmlFor="wideWidth">Wide Width?: </label>
                 <div />
-                <input className="half" required type="boolean" id="wideWidth" name="wideWidth" />
+                <input
+                  className="half"
+                  required
+                  type="boolean"
+                  id="wideWidth"
+                  name="wideWidth"
+                />
               </div>
             </div>
             <div className="space" />
 
-            <label htmlFor="school">
-              Teacher / School (if applicable)
-              {' '}
-            </label>
-            <input className="full" required type="string" id="school" name="school" />
+            <label htmlFor="school">Teacher / School (if applicable) </label>
+            <input
+              className="full"
+              required
+              type="string"
+              id="school"
+              name="school"
+            />
             <div className="space" />
-
-            <label htmlFor="notes">
-              Notes
-              {' '}
-            </label>
-            <input className="full" required type="string" id="notes" name="notes" />
-            <div className="space" />
-            <input type="submit" id="submit" name="submit" value="Add Request" />
+            <input
+              type="submit"
+              id="submit"
+              name="submit"
+              value="Add Request"
+            />
           </form>
 
-          <button type="submit" id="bigSubmit" name="bigsubmit" onClick={pushToAirtable}> Submit all orders </button>
-
-          <div style={errorStyle}>
-            {error}
-          </div>
-
-          <button type="button" onClick={printForm}>
-            Print Form
-          </button>
+          <div style={errorStyle}>{error}</div>
         </div>
       </div>
 
       <div className="column right">
         <div className="right-column">
-
           <div id="orders">
-            <h1>Active orders: </h1>
-            {uniqueActive.map((value) => (
-              <div>
-                <h2>
-                  <text>
-                    ID:
-                    {'\n\n\n\n'}
-                    {new Date(Date.parse(value)).toLocaleString('en-US')}
-                  </text>
-                  <text>
-                    {'\n\n\n\n'}
-                    Quantity:
-                    {cards.filter((card) => card.fields.Time === `${value}`).length}
-                  </text>
-                </h2>
-
-                <div className="container">
-                  <table>
-                    <thead>
-                      <tr>
-                        <th>Student&apos;s First Name and Last Name</th>
-                        <th>Age</th>
-                        <th>Gender</th>
-                        <th>Shoe Size</th>
-                        <th>Wide Width?</th>
-                        <th>Teacher or school?</th>
-                        <th>Notes or Comment</th>
+            <div className="container">
+              <table>
+                <thead>
+                  <tr>
+                    <th width="263px">
+                      Student&apos;s First Name and Last Name
+                    </th>
+                    <th width="100px">Age</th>
+                    <th width="60px">Gender</th>
+                    <th width="100px">Shoe Size</th>
+                    <th width="70px">Wide Width?</th>
+                    <th width="263px">Teacher or school?</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {console.log(curcards)}
+                  {curcards.length > 0
+                    && curcards.map((card, index) => (
+                      // eslint-disable-next-line react/no-array-index-key
+                      <tr key={index}>
+                        <td>{card.name}</td>
+                        <td>{card.age}</td>
+                        <td>{card.gender}</td>
+                        <td>{card.size}</td>
+                        <td>{card.wide}</td>
+                        <td>{card.school}</td>
                       </tr>
-                    </thead>
-                    <tbody>
-                      {cards.filter((card) => card.fields.Time === `${value}`).map((card, index) => (
-                        // eslint-disable-next-line react/no-array-index-key
-                        <tr key={index}>
-                          <td>{card.fields.Name}</td>
-                          <td>{card.fields.Age}</td>
-                          <td>{card.fields.Gender}</td>
-                          <td>{card.fields.Size}</td>
-                          <td>{card.fields.Wide}</td>
-                          <td>{card.fields['Teacher/School']}</td>
-                          <td>{card.fields.Active}</td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            ))}
+                    ))}
+                </tbody>
+              </table>
+              <div className="michael">
+                <button
+                  type="submit"
+                  id="bigSubmit"
+                  name="bigsubmit"
+                  onClick={pushToAirtable}
+                >
+                  {' '}
+                  Submit Form
+                </button>
 
-            <h1>Archived orders: </h1>
-            {uniqueInactive.map((value) => (
-              <div>
-                <h2>
-                  <text>
-                    ID:
-                    {'\n\n\n\n'}
-                    {new Date(Date.parse(value)).toLocaleString('en-US')}
-                  </text>
-                  <text>
-                    {'\n\n\n\n'}
-                    Quantity:
-                    {cards.filter((card) => card.fields.Time === `${value}`).length}
-                  </text>
-                </h2>
-
-                <div>
-                  {cards.filter((card) => card.fields.Time === `${value}`).map((card, index) => (
-                  // eslint-disable-next-line react/no-array-index-key
-                    <div key={index}>
-                      <p />
-                      <Card
-                        name={card.fields.Name}
-                        gender={card.fields.Gender}
-                        wideWidth={card.fields.Wide}
-                        size={card.fields.Size}
-                        age={card.fields.Age}
-                        school={card.fields['Teacher/School']}
-                        shoeSize={card.fields.Active}
-                      />
-                    </div>
-                  ))}
-                </div>
+                <button
+                  type="button"
+                  id="print"
+                  name="print"
+                  onClick={printForm}
+                >
+                  Print Form
+                </button>
               </div>
-            ))}
+            </div>
           </div>
         </div>
       </div>
