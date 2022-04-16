57d27eb799202add653bd8cb934ee4cbb9cb0dd5
Siddharth Nandy <sidd.nandy@gmail.com>
2022-04-16 11:37:34 -0700

    Nav bar icons (still ugly)

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {
+  Home, ClipboardList, ListOrdered, Gift, GraduationCap,
+} from 'lucide-react';
 import styles from './nav.module.css';
 
 export default function Nav() {
   return (
     <div className={styles.sidebar}>
-      <ul className={styles.navEntry}><Link to="/home" className={styles.linkStyles}>Home</Link></ul>
-      <ul className={styles.navEntry}><Link to="/inventory" className={styles.linkStyles}>Inventory</Link></ul>
-      <ul className={styles.navEntry}><Link to="/newshoeform" className={styles.linkStyles}>New Shoe Form</Link></ul>
-      <ul className={styles.navEntry}><Link to="/orderform" className={styles.linkStyles}>Order Form</Link></ul>
-      <ul className={styles.navEntry}><Link to="/adminlist" className={styles.linkStyles}>Admin List</Link></ul>
-      <ul className={styles.navEntry}><Link to="/admindashboard" className={styles.linkStyles}>Admin Dashboard</Link></ul>
+      <ul className={styles.navEntry}>
+        <Home color="#FFFFFF" />
+        <Link to="/home" className={styles.linkStyles}>Home</Link>
+      </ul>
+      <ul className={styles.navEntry}>
+        <ClipboardList color="#FFFFFF" />
+        <Link to="/inventory" className={styles.linkStyles}>Inventory</Link>
+
+      </ul>
+      <ul className={styles.navEntry}>
+        <ListOrdered color="#FFFFFF" />
+        <Link to="/newshoeform" className={styles.linkStyles}>New Shoe Form</Link>
+      </ul>
+      <ul className={styles.navEntry}>
+        <Gift color="#FFFFFF" />
+        <Link to="/orderform" className={styles.linkStyles}>Order Form</Link>
+      </ul>
+      <ul className={styles.navEntry}>
+        <GraduationCap color="#FFFFFF" />
+        <Link to="/adminlist" className={styles.linkStyles}>Admin List</Link>
+      </ul>
+      <ul className={styles.navEntry}>
+        <Home color="#FFFFFF" />
+        <Link to="/admindashboard" className={styles.linkStyles}>Admin Dashboard</Link>
+      </ul>
     </div>
   );
 }
