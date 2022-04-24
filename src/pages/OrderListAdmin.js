52f486d63fe139e4312c309691c6692ffc58fbe8
Laura <lauralu201@g.ucla.edu>
2022-04-23 20:36:56 -0700

    view history of one page
    yayy should be good now

--- src/pages/OrderListAdmin.js
+++ src/pages/OrderListAdmin.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import print from 'print-js';
 import './OrderListAdmin.css';
 
 const Airtable = require('airtable');
@@ -12,17 +13,29 @@ const base = new Airtable({ apiKey: airtableConfig.apiKey }).base(
   airtableConfig.baseKey,
 );
 
+function printForm() {
+  print({
+    printable: 'orders',
+    type: 'html',
+    // ignoreElements: ['print', 'bigSubmit'],
+    css: './OrderListAdmin.css',
+    targetStyles: ['*'],
+  });
+}
+
 function OrderListAdmin() {
   const [cards, setCards] = useState([]);
+  const [info, setInfo] = useState([]);
 
+  const id = 1234;
   const getCards = () => {
     base('Orders')
-      .select({ view: 'Grid view' })
+      .select({ filterByFormula: `id="${id}"` })
       .all()
       .then((records) => {
-        setCards(records);
+        setCards(JSON.parse(records[0].fields.Orders));
+        setInfo(records[0].fields);
       });
-    console.log(cards);
   };
 
   useEffect(() => {
@@ -30,47 +43,96 @@ function OrderListAdmin() {
   }, []);
 
   return (
-    <>
-      <div className="head">Active orders: </div>
-      {cards
-        .filter((card) => card.fields.ID === '2')
-        .map((card, index) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <div key={index}>
-            {/* <div className="hello">
-            {card.fields.Time}
-          </div> */}
-            {card.fields.Time}
-            <p />
-            <div className="container">
-              <table>
-                <thead>
-                  <tr>
-                    <th width="263px">
-                      Student&apos;s First Name and Last Name
-                    </th>
-                    <th width="100px">Age</th>
-                    <th width="60px">Gender</th>
-                    <th width="100px">Shoe Size</th>
-                    <th width="70px">Wide Width?</th>
-                    <th width="263px">Teacher or school?</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr key={JSON.stringify(index)}>
-                    <td>{card.fields.Name}</td>
-                    <td>{card.fields.Age}</td>
-                    <td>{card.fields.Gender}</td>
-                    <td>{card.fields.Size}</td>
-                    <td>{card.fields.Wide}</td>
-                    <td>{card.fields['Teacher/School']}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+    <div id="orders">
+      {/* {console.log(orders)} */}
+      {console.log(cards)}
+      {console.log(info)}
+      {/* {console.log(lines)} */}
+      {/* {console.log(jsonData)} */}
+      {/* {console.log(JSON.parse(cards))} */}
+      <div className="head">Order Details: </div>
+      <div className="wrapper">
+        <div>
+          <div className="title">
+            School Information
           </div>
-        ))}
-    </>
+          <p>{info.School}</p>
+          <p>{info.Address1}</p>
+          <p>
+            {info.City}
+            {' '}
+            {info.State}
+          </p>
+          <p>
+            {info['Zip Code']}
+          </p>
+        </div>
+        <div>
+          <div className="title">
+            Contact Information
+          </div>
+          <p>{info['Contact Name']}</p>
+          <p>{info['Email Address']}</p>
+          <p>
+            {info.Phone}
+          </p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="status">
+          {info && info.Active
+            ? <div className="statusChildInProgress">In Progress</div>
+            : <div className="statusChildFulfilled">Fulfilled</div>}
+        </div>
+        <div className="status">
+          Order placed on
+          {' '}
+          {info && info.Date}
+        </div>
+        <div className="status">
+          <button
+            type="button"
+            id="printform"
+            name="print"
+            onClick={printForm}
+          >
+            Print
+          </button>
+        </div>
+      </div>
+      <div className="container">
+        <table>
+          <thead>
+            <tr>
+              <th width="263px">Student&apos;s First Name and Last Name</th>
+              <th width="100px">Age</th>
+              <th width="60px">Gender</th>
+              <th width="100px">Shoe Size</th>
+              <th width="70px">Wide Width?</th>
+              <th width="263px">Teacher or school?</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cards.map((line) => (
+              <tr>
+                <td>{line.name}</td>
+                <td>{line.age}</td>
+                <td>{line.gender}</td>
+                <td>{line.size}</td>
+                <td>{line.wideWidth ? 'Yes' : 'No'}</td>
+                <td>{line.school}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="space" />
+        Notes
+        <div className="notes">
+          {info.Notes}
+          {' '}
+        </div>
+      </div>
+    </div>
   );
 }
 
